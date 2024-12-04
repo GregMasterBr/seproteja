496d5fa7bc41d81f1f43185f6ce90b6190c84ea2
GREGORIO DE ALMEIDA QUEIROZ <gregorio.queiroz@facens.br>
2024-12-04 16:19:50 -0300

    Add os demais componentes do site

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -1,6 +1,13 @@
+from django.contrib import admin
 from django.urls import path
 from  . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('estatisticas/', views.estatisticas, name='estatisticas'),
+    path('educacional/', views.educacional, name='educacional'),
+    path('denuncias/', views.denuncias, name='denuncias'),
+    path('delegacias-ciberneticas/', views.delegacias, name='delegacias'),
+    # path('admin/', admin.site.urls)
 ]
+
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -6,3 +6,21 @@ from django.contrib.messages import constants
 def home(request):
     messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
     return render(request,'index.html')
+
+def estatisticas(request):
+    messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
+    return render(request,'estatisticas.html')
+
+
+def educacional(request):
+    messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
+    return render(request,'educacional.html')
+
+
+def denuncias(request):
+    messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
+    return render(request,'denuncias.html')
+
+def delegacias(request):
+    messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
+    return render(request,'delegacias.html')
